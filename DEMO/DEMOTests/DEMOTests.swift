6e24baa31decb9193ef05c17b0895fa60b772b29
xiaoyou.qu <xiaoyouPrince@163.com>
2024-03-04 16:15:34 +0800

    update

--- DEMO/DEMOTests/DEMOTests.swift
+++ DEMO/DEMOTests/DEMOTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 @testable import DEMO
+import Modelable
 
 final class DEMOTests: XCTestCase {
 
@@ -24,6 +25,46 @@ final class DEMOTests: XCTestCase {
         // Any test you write for XCTest can be annotated as throws and async.
         // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
         // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+        
+        let jsonStr = """
+{
+  "FDnzT" : {
+    "nKIBe" : [
+      "asoak",
+      "VpADx",
+      "MuMbP"
+    ],
+    "knsKe" : true
+  },
+  "ZCJuS" : [
+    "USnLi",
+    "tPTLj",
+    "zIjyK"
+  ],
+  "Mcxbo" : 6662.3999999999996,
+  "jePaB" : false,
+  "BzgpO" : [
+    "zzWjA",
+    "hHkXT",
+    "XYfLj"
+  ],
+  "mlyTE" : true
+}
+"""
+        
+        if let data = jsonStr.data(using: .utf8),
+           let dict = try? JSONSerialization.jsonObject(with: data) {
+            print(dict)
+            XCTAssert(dict is [String: Any], "error")
+            
+            
+            let model = MyObj.mapping(jsonObject: dict)
+            XCTAssert(model != nil, "error")
+            print(model!)
+        }
+        
+        
+        
     }
 
     func testPerformanceExample() throws {
@@ -34,3 +75,19 @@ final class DEMOTests: XCTestCase {
     }
 
 }
+
+class FDnzT: Model {
+    var nKIBe: [String]?
+    var knsKe: Bool?
+}
+
+class MyObj: Model {
+    var FDnzT: FDnzT?
+    var ZCJuS: [String]?
+    var Mcxbo: Double?
+    var jePaB: Bool?
+    var BzgpO: [String]?
+    var mlyTE: Bool?
+    var vvv: CGFloat? = 0
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Modelable
 Codable 封装, 用于 JSON 转 Model
+
+## TODO
+
+1. 添加便捷方法: stirng -> JSONObj( dict / array)
+2. model.mapping(jsonString:)
+3. model.toJsonString (String by 移除code)
+
+## 用法
+
+## class
+
+## struct
+
+Modle key 数量小小于等于 JSON key 的数据量,
+否则转换失败, 本质原因: 从 JSON 中获取 key 的 value = nil, 整个 model 被设置为 nil
+
+处理方案:
+1. Modle.keys <= JSON.keys
+2. Keys 都设置为可选项, 这样对于解析到 nil 的内容, 就不会将整体设置为 nil
+
+
+## class 和 struct 选择
+如果你要对 model 来回修改建议选择 class, 例如场景: 编辑模块, 需要对被编辑对象来回修改操作
+其余场景都选择 struct
+官方说明: https://developer.apple.com/documentation/swift/choosing-between-structures-and-classes
+
+
+
