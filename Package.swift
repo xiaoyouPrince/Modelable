f9e70141f1707ff60214ad0a5cfc5d00cd1a2fe4
渠晓友 <xiaoyouPrince@163.com>
2024-08-01 15:08:25 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,19 +1,17 @@
-// swift-tools-version: 5.3
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
-
 import PackageDescription
 
 let package = Package(
     name: "Modelable",
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "Modelable",
             targets: ["Modelable"]),
     ],
+    dependencies: [
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "Modelable"),
         .testTarget(
@@ -21,5 +19,3 @@ let package = Package(
             dependencies: ["Modelable"]),
     ]
 )
-
-package.swiftLanguageVersions = [.v5]
