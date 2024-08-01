80bb7a8415253e22ba78b00f1986e397dda98dc8
xiaoyou.qu <xiaoyouPrince@163.com>
2024-08-01 09:41:58 +0800

    降低最低支持版本

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -21,3 +21,5 @@ let package = Package(
             dependencies: ["Modelable"]),
     ]
 )
+
+package.swiftLanguageVersions = [.v5]
